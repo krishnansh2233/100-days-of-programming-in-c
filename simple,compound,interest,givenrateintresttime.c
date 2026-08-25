922d8e345fa8bc4de9ef3c5f564bebf072af5d74
krishnansh2233 <krishnansh.42522@stu.upes.ac.in>
2026-08-25 19:59:36 +0530

    day 5

---
+++ simple,compound,interest,givenrateintresttime.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+	double principal, rate, time;
+	double simple_interest, compound_interest;
+	scanf("%lf %lf %lf", &principal, &rate, &time);
+	simple_interest = principal * rate * time / 100.0;
+	compound_interest = principal * (pow(1.0 + rate / 100.0, time) - 1.0);
+
+	printf("Simple Interest=%g, Compound Interest=%.2f\n",
+		   simple_interest, compound_interest);
+	return 0;
+}

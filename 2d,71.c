7c3f259e244b4638ca9d6eba9a96d329994045a9
krishnansh2233 <krishnansh.42522@stu.upes.ac.in>
2026-09-16 00:02:24 +0530

    day 36

---
+++ 2d,71.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j, sum = 0;
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j];
+        }
+    }
+
+    printf("%d", sum);
+    return 0;
+}

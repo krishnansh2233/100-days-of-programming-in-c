b52a25a41af31b046afe1b9f3ef839f9f69e373c
krishnansh2233 <krishnansh.42522@stu.upes.ac.in>
2026-09-15 19:17:11 +0530

    day 14

---
+++ product,even,1TOn.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int n;
+	long long product = 1;
+	scanf("%d", &n);
+	for (int i = 2; i <= n; i += 2) {
+		product *= i;
+	}
+	printf("%lld\n", product);
+	return 0;
+}

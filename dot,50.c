ddb3842128e830c44cbbd2f669e5b2cff81500e7
krishnansh2233 <krishnansh.42522@stu.upes.ac.in>
2026-09-15 22:59:19 +0530

    day 25

---
+++ dot,50.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < i; j++) {
+			printf(" ");
+		}
+		for (int j = 0; j < 5 - i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
